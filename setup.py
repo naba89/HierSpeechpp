57e22dc721793095b9fb39ceb907f29c782631db
Nabarun Goswami <nabarungoswami@mi.t.u-tokyo.ac.jp>
2024-07-17 19:37:12 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open("requirements.txt") as f:
     required = f.read().splitlines()
@@ -11,23 +11,6 @@ setup(
     description="Hierspeech++ pip installable",
     author="Nabarun Goswami",
     author_email="nabarungoswami@mi.t.u-tokyo.ac.jp",
-    packages=["hierspeechpp"],
+    packages=find_packages(),
     install_requires=required
-    # [
-    #     "AMFM_decompy",
-    #     "Cython",
-    #     "einops",
-    #     "joblib",
-    #     "matplotlib",
-    #     "numpy",
-    #     "pesq",
-    #     "phonemizer",
-    #     "scipy",
-    #     "timm",
-    #     "torch",
-    #     "torchaudio",
-    #     "tqdm",
-    #     "transformers",
-    #     "Unicode",
-    # ]
 )
